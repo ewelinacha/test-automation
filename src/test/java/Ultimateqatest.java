da52ed00c0b00acfa5f35ff29c85e0039d52f674
Ewelina Chasikowska <ewciachyl@poczta.fm>
2023-03-19 17:06:20 +0100

    first commit - project created

--- src/test/java/Ultimateqatest.java
+++ src/test/java/Ultimateqatest.java
@@ -1,17 +1,42 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Ultimateqatest {
     String projectLocation = System.getProperty("user.dir");
     String UltimateQAURL = "https://ultimateqa.com/simple-html-elements-for-automation/";
 
-    @Test
-    public void testOne()
-    {
+    WebDriver driver;
+
+    @BeforeEach
+    public void Setup() {
         System.out.println(projectLocation);
         System.setProperty("webdriver.chrome.driver", projectLocation + "/external resources/chromedriver.exe");
 
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.get(UltimateQAURL);
+        driver.manage().window().fullscreen();
+    }
+
+    @AfterEach
+    public void teardown() {
+        //driver.close();
+    }
+
+    @Test
+    public void testOne()
+    {
+        WebElement button = driver.findElement(By.id("idExample"));
+        button.click();
+
+        WebElement buttonSuccess = driver.findElement(By.className("entry-title"));
+        assertEquals("Button success", buttonSuccess.getText());
     }
 }
